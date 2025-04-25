6ba1309fa254a16b6613dafa5e9b3b9bac1b4159
Chema Flores <chemaflores92@gmail.com>
2025-04-25 02:56:59 +0200

    fixed

--- src/components/jugador/ListaJugadores.js
+++ src/components/jugador/ListaJugadores.js
@@ -24,7 +24,7 @@ async function ListaJugadores({ entrenamientoId, disabled }) {
                                     type='checkbox'
                                     name={jugador.id}
                                     value={jugador.id}
-                                    checked={jugador.id === jugadorEntreno}
+                                    defaultChecked={jugador.id === jugadorEntreno}
                                     className="mr-2"
                                 />
                                 <span className="flex-grow">{jugador.nombre} {jugador.apellido}</span>
